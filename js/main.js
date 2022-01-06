024e4efeded7497a822fc57f2bfa362b63bb12ab
filosof777 <olimjonkamalov@mail.ru>
2022-01-06 14:02:46 +0500

    initail commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 let elFormInput = document.querySelector(".header-form__input");
 let elIconSearch = document.querySelector(".header-form__icon-search");
 let elIconClose = document.querySelector(".header-form__icon-close"); 
+let elPostBtn = document.querySelectorAll('.post__btn');
+let elContentList = document.querySelector('.content__list');
+let elModalBlog = document.querySelector('.modal-blogs');
+let elModalWrapper = document.querySelector('.modal-wrap');
+
+
+
+elPostBtn.forEach(item => {
+  item.addEventListener('click', () => {
+    elPostBtn.forEach(items => {
+      items.classList.remove('post__item--active')
+    })
+    item.classList.add('post__item--active')
+  })
+});
 
 elFormInput.addEventListener('mouseup', () => {
   elIconSearch.classList = ('d-none')
@@ -17,3 +32,206 @@ elFormInput.addEventListener('mouseover', () => {
 elFormInput.addEventListener('keyup', () => {
   console.log(elFormInput.value);
 });
+
+arr.forEach(item => {
+  if (item.id == 'image') {
+    elContentList.innerHTML += `
+    <li class="content__item" id="image">
+      <div class="content__inner">
+        <div class="content__wrapper">
+          <span class="content__like">
+            <i class="fas fa-heart"></i>${item.likeCount}
+          </span>
+          <span class="content__coment">
+            <i class="fas fa-comment"></i>${item.commentCount}
+          </span>
+        </div>
+      </div>
+      <img src="${item.src}" alt="img">
+    </li>
+    `
+  }
+  if (item.id == 'video') {
+    elContentList.innerHTML += `
+    <li class="content__item" id="video">
+      <i class="content__icons fas fa-play"></i>
+      <div class="content__inner">
+        <div class="content__wrapper">
+          <span class="content__like">
+            <i class="fas fa-heart"></i>${item.likeCount}
+          </span>
+          <span class="content__coment">
+            <i class="fas fa-comment"></i>${item.commentCount}
+          </span>
+        </div>
+      </div>
+      <video>
+        <source src="${item.src}" type="video/mp4">
+      </video>
+    </li>
+    `
+  }
+  showModal();
+})
+
+function showModal(){
+  let elContentItem = document.querySelectorAll('.content__item');
+
+elContentItem.forEach(item => {
+  item.addEventListener('click', (e) => {
+    if (e.target.id == 'image') {
+      let imgSrc = e.target.querySelector('img')
+      elModalBlog.innerHTML = `
+      <div class="modal-wrap">
+        <div class="row">
+          <div class="modal-wrap__col px-0 col-7 d-flex align-items-center justify-content-center">
+            <img class="modal-wrap__img" src="${imgSrc.src}" alt="img">            
+          </div>
+          <div class="modal-wrap__col modal-wrap__comments col-5 px-0 ">
+
+          </div>
+        </div>
+      </div>
+      `;
+      likeComment()
+    }
+    if (e.target.id == 'video') {
+      let videoSrc = e.target.querySelector('source');
+      elModalBlog.innerHTML = `
+      <div class="modal-wrap">
+        <div class="row">
+          <div class="modal-wrap__col px-0 col-7 d-flex align-items-center justify-content-center">
+          <video controls>
+            <source src="${videoSrc.src}" type="video/mp4">
+          </video>          
+          </div>
+          <div class="modal-wrap__col modal-wrap__comments col-5 px-0 ">
+
+          </div>
+        </div>
+      </div>
+      `;
+      likeComment();
+    }
+  })
+})
+}
+
+elPostBtn.forEach(item => {
+  item.addEventListener('click', (e) => {
+    if (e.target.value == 'video') {
+      elContentList.innerHTML = "";
+      arr.forEach(item => {
+        if (item.id == 'video') {
+          elContentList.innerHTML += `
+          <li class="content__item" id="video">
+            <i class="content__icons fas fa-play"></i>
+            <div class="content__inner">
+              <div class="content__wrapper">
+                <span class="content__like">
+                  <i class="fas fa-heart"></i>${item.likeCount}
+                </span>
+                <span class="content__coment">
+                  <i class="fas fa-comment"></i>${item.commentCount}
+                </span>
+              </div>
+            </div>
+            <video>
+              <source src="${item.src}" type="video/mp4">
+            </video>
+          </li>
+          `
+        }
+      })
+      showModal();
+    }
+    if (e.target.value == 'image') {
+      elContentList.innerHTML = "";
+      arr.forEach(item => {
+        if (item.id == 'image') {
+          elContentList.innerHTML += `
+          <li class="content__item" id="image">
+            <div class="content__inner">
+              <div class="content__wrapper">
+                <span class="content__like">
+                  <i class="fas fa-heart"></i>${item.likeCount}
+                </span>
+                <span class="content__coment">
+                  <i class="fas fa-comment"></i>${item.commentCount}
+                </span>
+              </div>
+            </div>
+            <img src="${item.src}" alt="img">
+          </li>
+          `
+        }
+        showModal()
+      })
+    }
+    if (e.target.value == 'all-post') {
+      createElement()
+    }
+  })
+
+})
+
+function createElement() {
+  elContentList.innerHTML = "";
+
+  arr.forEach(item => {
+    if (item.id == 'image') {
+      elContentList.innerHTML += `
+      <li class="content__item" id="image">
+        <div class="content__inner">
+          <div class="content__wrapper">
+            <span class="content__like">
+              <i class="fas fa-heart"></i>${item.likeCount}
+            </span>
+            <span class="content__coment">
+              <i class="fas fa-comment"></i>${item.commentCount}
+            </span>
+          </div>
+        </div>
+        <img src="${item.src}" alt="img">
+      </li>
+      `
+    }
+    if (item.id == 'video') {
+      elContentList.innerHTML += `
+      <li class="content__item" id="video">
+        <i class="content__icons fas fa-play"></i>
+        <div class="content__inner">
+          <div class="content__wrapper">
+            <span class="content__like">
+              <i class="fas fa-heart"></i>${item.likeCount}
+            </span>
+            <span class="content__coment">
+              <i class="fas fa-comment"></i>${item.commentCount}
+            </span>
+          </div>
+        </div>
+        <video>
+          <source src="${item.src}" type="video/mp4">
+        </video>
+      </li>
+      `
+    }
+    showModal();
+  })
+}
+
+createElement()
+
+function likeComment() {
+  let elModalComment = document.querySelector('.modal-wrap__comments');
+  comment.forEach(item => {
+    elModalComment.innerHTML += `
+    <div class="mb-4 d-flex align-items-center">
+      <img class="modal-wrap__user-img" src="${item.src}" alt="">
+      <a class="modal-wrap__user-name" href="#">${item.userName}</a>
+      <p class="modal-wrap__comment mb-0">${item.massages}</p>
+      <i class="modal-wrap__like fal fa-heart"></i>
+    </div>
+    `
+  })
+} 
---
+++ js/post.js
@@ -0,0 +1,56 @@
+let arr = [
+  {
+    id: "image",
+    src: "img/img-1.jpg",
+    likeCount: 15,
+    commentCount: 10
+  },
+  {
+    id: "video",
+    src: "img/video-3.mp4",
+    likeCount: 4,
+    commentCount: 6
+  },
+  {
+    id: "image",
+    src: "img/img-2.jpg",
+    likeCount: 88,
+    commentCount: 12
+  },
+  {
+    id: "image",
+    src: "img/img-3.jpg",
+    likeCount: 15,
+    commentCount: 1
+  },
+  {
+    id: "image",
+    src: "img/img-4.jpg",
+    likeCount: 44,
+    commentCount: 85
+  },
+  {
+    id: "video",
+    src: "img/video-1.mp4",
+    likeCount: 42,
+    commentCount: 33
+  },
+  {
+    id: "image",
+    src: "img/img-5.jpg",
+    likeCount: 400,
+    commentCount: 21
+  },
+  {
+    id: "video",
+    src: "img/video-2.mp4",
+    likeCount: 555,
+    commentCount: 1
+  },
+  {
+    id: "image",
+    src: "img/img-6.jpg",
+    likeCount: 16,
+    commentCount: 6
+  }
+]
